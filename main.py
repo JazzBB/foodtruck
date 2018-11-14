910f3ea8a692b250ca72ab2ba1fab31f8d02c3b9
JazzBB <30501653+JazzBB@users.noreply.github.com>
2018-11-14 16:20:35 +0100

    random indexelés plusz translate a címre

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import requests
 import json
 import objectpath
 from py_translator import Translator
+from random import randint
 
-APPID = "APPID"
-KULCS = "APIKEY"
-response = requests.get("https://api.edamam.com/search?q=beef+eggplant&app_id="+APPID+"&app_key="+KULCS+"&from=0&to=1&diet=low-carb&nutrients%5BCHOCDF%5D=45-50").json()
-#parsed_response = json.loads(response)
+APPID = ""
+KULCS = ""
+index_from = randint(1,4)
+index_to = index_from - 1
+if index_to < index_from:
+    index_to = index_to + 2
+response = requests.get("https://api.edamam.com/search?q=fish+lemon&app_id="+APPID+"&app_key="+KULCS+"&from="+str(index_from)+"&to="+str(index_to)+"&diet=low-carb&nutrients%5BCHOCDF%5D=45-50").json()
 
 #Adatok kiszedése a JSONbol
 response_tree = objectpath.Tree(response['hits'])
@@ -16,9 +20,9 @@ recept_neve = tuple(response_tree.execute('$..label'))
 hozzavalok = tuple(response_tree.execute('$..ingredients'))
 fozesi_ido = tuple(response_tree.execute('$..totalTime'))
 kaloria = tuple(response_tree.execute('$..ENERC_KCAL'))
-
 #print, aztan majd megy GUIra
-print(recept_neve[0])
+
+print(Translator().translate(text=recept_neve[0],dest='hu').text)
 print(szemely[0])
 print("Össz szénhidrát tartalom:"+ str(szenhidrat[0]['quantity'])+ str(szenhidrat[0]['unit']))
 egy_adag_ch = int(szenhidrat[0]['quantity'])/int(szemely[0])
@@ -28,4 +32,3 @@ print("Főzési idő: "+str(fozesi_ido[0]))
 print("Hozzávalók\n----------------------------------------------------------------")
 for hozzavalok_db in hozzavalok:
     print(Translator().translate(text=hozzavalok_db['text'],dest='hu').text)
-#print("recept neve: "+recept_neve+"\n"+"Szénhidrát tartalom(össz): "+szenhidrat)
